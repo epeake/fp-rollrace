ad860e8c4692433cb7b35cc294b90a888317b4b2
“[Elijah <elijah.peake@gmail.com>
2019-04-05 16:16:02 -0400

    issue #54 fixed

--- client/src/GameCharacter.js
+++ client/src/GameCharacter.js
@@ -14,7 +14,6 @@ class GameCharacter extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      jumpStartTime: undefined,
       x: 400,
       y: 400,
       yStart: 400,
@@ -23,27 +22,36 @@ class GameCharacter extends Component {
       windowHeight: window.innerHeight
     };
 
-    this.temporaryState = {
-      windowWidth: window.innerWidth,
-      windowHeight: window.innerHeight
-    };
+    this.jumpStartTime = null;
+
+    /*
+     * I don't know why, but the event gets messed up after rerendering if
+     * timeout is not in the constructor...
+     */
+    this.timeout = null;
 
     this.debounce = this.debounce.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
     this.handleWindowResize = this.handleWindowResize.bind(this);
   }
 
-  // Prevent functions from event handlers from being repeatedly called
+  /*
+   * Prevent functions from event handlers from being repeatedly called.
+   * This is essential for on window resize, which could potentially be called
+   * a ton.
+   *
+   * Params: func: function to debounce
+   *         delay: how long to wait after the last call until we fire func
+   *         ...args: func's arguements
+   */
   debounce(func, delay, ...args) {
-    let timeout;
-
     return () => {
       let fireLater = () => {
         func(...args);
       };
 
-      clearTimeout(timeout);
-      timeout = setTimeout(fireLater, delay);
+      clearTimeout(this.timeout);
+      this.timeout = setTimeout(fireLater, delay);
     };
   }
 
@@ -52,10 +60,10 @@ class GameCharacter extends Component {
     // TODO: promise so excecutes when hits bottom?
     if (event.keyCode === 32 && this.state.jumpState === jump.STOP) {
       // so they can't jump mid jump
+      this.jumpStartTime = new Date().getTime();
       this.setState({
         yStart: this.state.y,
-        jumpState: jump.UP,
-        jumpStartTime: new Date().getTime()
+        jumpState: jump.UP
       });
     } else {
       void 0; // do nothing
@@ -64,7 +72,7 @@ class GameCharacter extends Component {
 
   // Resets our current window dimentions
   handleWindowResize() {
-    Object.assign(this.temporaryState, {
+    this.setState({
       windowWidth: window.innerWidth,
       windowHeight: window.innerHeight
     });
@@ -76,25 +84,27 @@ class GameCharacter extends Component {
     if (this.state.jumpState !== jump.STOP) {
       // mid jump
       const currentTime = new Date().getTime();
-      if (currentTime - this.state.jumpStartTime < JUMP_TIME) {
+      if (currentTime - this.jumpStartTime < JUMP_TIME) {
         setTimeout(() => {
           this.setState({
             y:
               this.state.yStart -
               Math.abs(
                 Math.abs(
-                  ((currentTime - this.state.jumpStartTime) / JUMP_TIME) *
+                  ((currentTime - this.jumpStartTime) / JUMP_TIME) *
                     2 *
                     JUMP_HEIGHT -
                     JUMP_HEIGHT
                 ) - JUMP_HEIGHT
               )
           });
-        }, UPDATE_TIMEOUT);
-      }
+        }, UPDATE_TIMEOUT); // timeout controls animation speed
+      } else {
 
-      // stop jump when jump should be over and return the sprite to the original prejump location
-      else {
+      /*
+       * stop jump when jump should be over and return the sprite to the
+       * original prejump location
+       */
         this.setState({ jumpState: jump.STOP, y: this.state.yStart });
       }
     }
