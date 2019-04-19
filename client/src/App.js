9082856e00048bbbc26e59b724614b826f129c99
Diaz <bddiaz@middlebury.edu>
2019-04-19 11:36:04 -0400

    added main menu branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,17 @@
 import React, { Component } from 'react';
 import GameEngine from './GameEngine.js';
+import styled from 'styled-components';
+import realbutton from './buttonSVGs/realPlaybutton.svg';
+import settingsbutton from './buttonSVGs/settingsbutton.svg';
+import statsbutton from './buttonSVGs/statsbutton.svg';
+import { ReactComponent as title } from './buttonSVGs/title.svg';
+import './App.css';
+import Settings from './settings.js';
+import Statistics from './Statistics.js';
+
+const StyledTitle = styled(title)`
+  height: 100;
+`;
 
 /* eslint-disable react/prefer-stateless-function */
 class App extends Component {
@@ -9,18 +21,76 @@ class App extends Component {
       map: [
         'm 0, 442 h 159 v -79 h 159 v 79 h 95 v 95 h 143 v -95 h 381 v -95 h 159 v 95 h 238 v -95 h 365 v -95 h 286 v -95 h 143 v 413 h 333 v -95 h 603 v 95 h 238 v -79 h 143 v 175 h 127 v -79 h 143 v -95 h 111 v 16 h 429 v -143 h 111 v 143 h 333 v -111 h 127 v 111 h 270 v 143 h 143 v -79 h 79 v -79 h 238 v -127 h 175 v 127 h 143 v -95 h 127 v 238 h 159 v -111 h 270 v -127 h 159 v 175 h 238 v -111 h 190 v 95 h 127 v -127 h 397 v -127 h 190 v 190 h 206 v -95 h 111 v 79 h 127 v -111 h 111 v 143 h 95 v -127 h 127 v 143 h 127 v -127 h 127 v 318 h 460 v -175 h 127 v 143 h 111 v -222 h 333 v -127 h 412 v -300 h 20'
       ],
-      strokeWidth: 12 // must be an even number for the parsing algorithm
+      strokeWidth: 12, // must be an even number for the parsing algorithm
+      mode: 'menu'
     };
+    this.handleGoToMenu = this.handleGoToMenu.bind(this);
+  }
+
+  handleGoToMenu() {
+    this.setState({ mode: 'menu' });
   }
   render() {
-    return (
-      <GameEngine
-        mapProps={Object.assign(
-          {},
-          { map: this.state.map, strokeWidth: this.state.strokeWidth }
-        )}
-      />
-    );
+    const { mode } = this.state;
+    if (mode === 'game') {
+      return (
+        <GameEngine
+          mapProps={Object.assign(
+            {},
+            { map: this.state.map, strokeWidth: this.state.strokeWidth }
+          )}
+        />
+      );
+    }
+    if (mode === 'menu') {
+      return (
+        <div>
+          <StyledTitle />
+          <div class="play-button">
+            <img
+              src={realbutton}
+              height="50"
+              className="playlogo"
+              alt="play"
+              onClick={() => this.setState({ mode: 'game' })}
+            />
+            <div class="settings-button">
+              <img
+                src={settingsbutton}
+                height="50"
+                className="settinsglogo"
+                alt="settings"
+                onClick={() => this.setState({ mode: 'settings' })}
+              />
+              <div>
+                <img
+                  src={statsbutton}
+                  height="50"
+                  className="statslogo"
+                  alt="stats"
+                  onClick={() => this.setState({ mode: 'stats' })}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+
+    if (mode === 'settings') {
+      return (
+        <div>
+          <Settings GoToMenu={this.handleGoToMenu} />
+        </div>
+      );
+    }
+    if (mode === 'stats') {
+      return (
+        <div>
+          <Statistics GoToMenu={this.handleGoToMenu} />
+        </div>
+      );
+    }
   }
 }
 
