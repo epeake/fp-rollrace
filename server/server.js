9eb62a16710a25f176397ec6bce38bd28e269841
“[Elijah <elijah.peake@gmail.com>
2019-04-23 22:43:39 -0400

    fixed travis

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const knexConfig = require('./knexfile');
 const knex = require('knex')(knexConfig[process.env.NODE_ENV || 'development']);
