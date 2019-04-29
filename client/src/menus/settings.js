975cf463ee3eb2e0a40e924b53995239ceec8df1
Bryan Diaz <bddiaz@middlebury.edu>
2019-04-29 19:37:21 -0400

    can change players color with sliders

--- client/src/menus/settings.js
+++ client/src/menus/settings.js
@@ -4,19 +4,20 @@ import settings from '../buttonSVGs/settings.svg';
 import return2menu from '../buttonSVGs/return2menu.svg';
 import styled from 'styled-components';
 
-const ColorBox = styled.div`
-  width: 50px;
-  height: 50px;
-  border: 1px solid black;
-  margin: auto;
-  background: ${props => `rgb(${props.red},${props.green},${props.blue})`};
-`;
-
 const ColorLabel = styled.div`
   display: inline-block;
   width: 50px;
   text-align: left;
   margin: auto;
+`;
+const StyledButton = styled.img`
+  display: block;
+  margin: auto;
+  float: right;
+`;
+const StyledCircle = styled.circle`
+  margin: auto;
+  display: center;
 `;
 
 function LabeledSlider(props) {
@@ -41,6 +42,7 @@ class Settings extends Component {
     this.state = { red: 0, green: 0, blue: 0 };
     this.handleClick = this.handleClick.bind(this);
     this.selectColor = this.selectColor.bind(this);
+    this.makeColor = this.makeColor.bind(this);
   }
   handleClick() {
     this.props.goToMenu();
@@ -51,15 +53,32 @@ class Settings extends Component {
     this.props.selectedColor(color);
   }
 
+  makeColor() {
+    let color = `rgb(${this.state.red},${this.state.green},${this.state.blue})`;
+    return color;
+  }
+
   render() {
     return (
       <div>
         <img src={settings} className="settingslogo" alt="settings" />
         <div>
-          <ColorBox
-            red={this.state.red}
-            green={this.state.green}
-            blue={this.state.blue}
+          <svg height="100" width="100">
+            <StyledCircle
+              cx="50"
+              cy="50"
+              r="40"
+              stroke="black"
+              stroke-width="1"
+              fill={this.makeColor()}
+            />
+          </svg>
+          <StyledButton
+            src={return2menu}
+            height="50"
+            className="2menubutton"
+            alt="2menu"
+            onClick={this.handleClick}
           />
           <LabeledSlider
             label="Red"
@@ -84,15 +103,6 @@ class Settings extends Component {
           />
           <button onClick={this.selectColor}> Select Color </button>
         </div>
-        <div>
-          <img
-            src={return2menu}
-            height="50"
-            className="2menubutton"
-            alt="2menu"
-            onClick={this.handleClick}
-          />
-        </div>
       </div>
     );
   }
@@ -106,5 +116,5 @@ LabeledSlider.propTypes = {
 Settings.propTypes = {
   goToMenu: PropTypes.func.isRequired
 };
-export { ColorBox };
+
 export default Settings;
